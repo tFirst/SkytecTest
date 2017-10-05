a6dec039e786d1973faaffe7798bf6fbe50da70e
tFirst <valtario349@gmail.com>
2017-10-05 18:55:35 +0700

    Add the index page

--- src/main/java/com/skytec/test/web/UserController.java
+++ src/main/java/com/skytec/test/web/UserController.java
@@ -11,18 +11,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/")
-public class UserController {
+public class UserController extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "index");
+
+        registry.addViewController("/index")
+                .setViewName("/index");
+    }
 
     @Autowired
     private UserRepository userRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> showAll() {
+        User user = new User();
+        user.setLogin("john");
+        user.setPassword("pass");
+        userRepository.save(user);
         return new ResponseEntity<>
                 (userRepository.findAll(), HttpStatus.OK);
     }
